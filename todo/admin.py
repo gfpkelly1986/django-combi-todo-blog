8b4a82c60fb211389882f050630dbf6f8f03d9b3
gfpkelly1986 <gfpkelly@gmail.com>
2022-09-27 13:15:39 +0000

    Add a basic model, register with admin, override str

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Item
 
 # Register your models here.
+# Functional example first
+
+admin.site.register(Item)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# blank == required on forms
+# null == cannot be empty programatically
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    # This is how to override the str method from the inherited Model class.
+    # This is a great example of the benefits of class based inheritence at play.
+    def __str__(self):
+        return self.name
