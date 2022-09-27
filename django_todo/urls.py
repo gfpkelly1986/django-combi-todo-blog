27ed30af75710990da7f3d34930a41cbb5f6395f
gfpkelly1986 <gfpkelly@gmail.com>
2022-09-27 16:49:07 +0000

    Set up a basic form, Begin C in CRUD functionality

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from todo.views import get_todo_list
+from todo.views import get_todo_list, add_item
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +23,5 @@ urlpatterns = [
     # change when combining projects together.
     path('', get_todo_list, name='home'),
     path('summernote/', include('django_summernote.urls')),
+    path('add/', add_item, name='add')
 ]
