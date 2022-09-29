d4eb8027b0d770acd26fd1aa3e1c448845b1d4e9
gfpkelly1986 <gfpkelly@gmail.com>
2022-09-29 13:25:55 +0000

    Complete POST functionality on comment form in post-detail

--- todo/views.py
+++ todo/views.py
@@ -38,6 +38,8 @@ class PostList(generic.ListView):
     paginate_by = 6
 
 
+# --- When using class based views, 
+# get and post are supplied as methods of the class --- 
 class PostDetail(View):
 
     def get(self, request, slug, *args, **kwargs):
@@ -54,6 +56,38 @@ class PostDetail(View):
             {
                 'post': post,
                 'comments': comments,
+                'commented': False,
+                'liked': liked,
+                'comment_form': CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            'post_detail.html',
+            {
+                'post': post,
+                'comments': comments,
+                'commented': True,
                 'liked': liked,
                 'comment_form': CommentForm()
             },
